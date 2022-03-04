394093b62933fb9ce52072e6a35e4c8791548c16
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2022-03-04 23:53:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace tipdonusumleri{
+    class Program{
+   
+        public static  void Main (string[]args)
+        {
+
+            //Implicit Conversion
+
+            byte a=5;
+            sbyte b=30;
+            short c=10;
+
+            int d=a+b+c;
+            Console.WriteLine("d:"+d);
+
+            long h=d;
+            Console.WriteLine("h:"+h);
+            
+            float i=h;             
+            Console.WriteLine("i"+i);
+            string e="Betül";
+            char f='k';
+            object g=e+f+d;
+            Console.WriteLine("g:"+g);
+            
+            //Explicit Conversion
+            int x=4;
+            byte y=(byte)x;
+            Console.WriteLine("y:"+y);
+            int z=100;
+            byte r=(byte)z;
+            Console.WriteLine("r:"+r);
+            float w=10.3f;
+            byte v=(byte)w;
+            Console.WriteLine("v:"+v);
+            //ToString
+            int xx=6;
+            string yy=xx.ToString();
+            Console.WriteLine("yy:"+yy);
+            string zz=12.5f.ToString();
+            Console.WriteLine("zz:"+zz);
+            //SystemConvert
+            string s1="10",s2="20";
+            int sayi1,sayi2;
+            int Toplam;
+            sayi1=Convert.ToInt32(s1);
+            sayi2=Convert.ToInt32(s2);
+            Toplam=sayi1+sayi2;
+            Console.WriteLine("Toplam:"+Toplam);
+            //Parse
+            parseMethod();
+        }
+        public static void parseMethod(){
+            string metin1="10";
+            string metin2="10.25";
+            int rakam1;
+            double double1;
+            rakam1=Int32.Parse(metin1);
+            double1=Double.Parse(metin2);
+            Console.WriteLine("rakam1:"+rakam1);
+            Console.WriteLine("double1:"+double1);
+        }
+    }
+}
